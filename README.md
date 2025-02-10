9432ec38d5fd92b814a14a985e140ff0ffdfd3f1
discomanfulanito <pablonecpas@gmail.com>
2025-02-10 02:29:41 +0100

    wv, code rework, optimize, clean and with separated functions

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
 # Miau Miau dict
 
 ## TODO
-audio de las frases enteras
 guardar words/phrases dependiendo de el boton
 
 no añadir palabras conocidas
-
-css options page
-
 crear deck si no existe
--- src/component/Popup.jsx
+++ src/component/Popup.jsx
@@ -44,7 +44,7 @@ function Popup() {
         });
         
         chrome.storage.local.get("miauPrompt", async (data)=> {
-          const def = await fetchDefinition(selectedWord, sentence, data.miauPrompt);
+          const def = await getDefinition(selectedWord, sentence, data.miauPrompt);
           setDefinition(def);
         });
 
@@ -78,22 +78,97 @@ function Popup() {
     return text.slice(start, end).trim();
   };
 
-  const fetchDefinition = async (word, sentence, structure) => {
+  // /define/ endpoint related
+  const fetchCompletion = async (word, sentence, structure) =>
+  {
+    const response = await fetch("https://miau-miau-dict-backend.onrender.com/define/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ word, sentence, structure}),
+    });
+    const result = await response.json()
+    const completion = await result[0]
+    setCodeLang(completion.c)
+    return completion.d
+  }
+  const getDefinition = async (word, sentence, structure) => {
     try {
-      const response = await fetch("https://miau-miau-dict-backend.onrender.com/define/", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ word, sentence, structure}),
-      });
-      const data = await response.json();
-      const clean_data = await data[0]
-      setCodeLang(clean_data.c)
-      return clean_data.d;
+      const def = await fetchCompletion(word, sentence, structure)
+      return def
     } 
     catch (error) {
       return "Error al obtener la definición.";
     }
   };
+
+
+  // /tts/ endpoint related 
+  const fetchAudio = async (text, code) => {
+    
+    const audioResponse = await fetch("https://miau-miau-dict-backend.onrender.com/tts/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({text, code}),
+    });
+    const audioResult = await audioResponse.json();
+    if (audioResult.audio) return await audioResult.audio
+  }
+  const getAudio = async (text, code) => {
+    try {
+      const audio = await fetchAudio(text, code)
+      return audio
+    }    catch (error)
+    {console.error("Error while getting audio:", error)}
+  }
+  
+
+  // localhost:8765 (anki) related
+  const storeMediaFiles = async (filename, file) => {
+    const storeMediaResponse = await fetch("http://localhost:8765", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        action: "storeMediaFile",
+        version: 6,
+        params: { filename: filename, data: file },
+      }),
+    });
+    const storeMediaResult = await storeMediaResponse.json();
+    if (!storeMediaResult.result) console.error("Error while storing media files")
+  }
+  const addNote = async (deckName, audioFilename, word, back) => {
+    try{
+      const addNoteResponse = await fetch("http://localhost:8765", { 
+        method: "POST", 
+        headers: { "Content-Type": "application/json" }, 
+        body: JSON.stringify({
+          "action": "addNote",
+          "version": 6,
+          "params": {
+            "note": {
+              "deckName": deckName,
+              "modelName": "Básico",
+              "fields": {
+                "Anverso": `[sound:${audioFilename}] ${word}`,
+                "Reverso": back
+              },
+              "tags": ["miaumiau"],
+              "options": {
+                "allowDuplicate": false
+              }
+            }
+          }
+        }
+        )
+      });
+      const addNoteResult = await addNoteResponse.json();
+      return await addNoteResult.result
+    } catch(error) {
+      console.error("Error while fetching localhost:8765 (anki connect api), MAKE SURE TO HAVE ANKI OPENED ;)")
+    }
+  }
+
+
   const play_audio = (audio64) => {
     const binaryData = atob(audio64); // Decodificar base64 a binario
         const arrayBuffer = new Uint8Array(binaryData.length);
@@ -108,117 +183,43 @@ function Popup() {
         const audio = new Audio(url);
         audio.play();
   }
+
+
+  // handle buttons
   const handleSound = async () => {
-
-    if (audio)
-    {
-      play_audio(audio);
-      console.log("no new audio generated")
-      return null;
+    if (audio) play_audio(audio);
+    else {
+      const newAudio = await getAudio(selectedText, codeLang)
+      setAudio(newAudio)
+      play_audio(newAudio)
     }
-    try {
-      const response = await fetch("https://miau-miau-dict-backend.onrender.com/tts/", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ text: selectedText, code:codeLang}),
-      });
-  
-      const data = await response.json();
-      if (data.audio) {
-        setAudio(data.audio)
-        play_audio(data.audio)
-        console.log("new audio generated")
-      }
-
-  } catch(error)
-  {console.log("error while handling sound",error)}
-  }
-  
+  }  
   const handleAdd = async () => {
     try {
       chrome.storage.local.get(["miauDeckInput", "miauAnkiInput"], async (data)=> {
-      const structure = data.miauAnkiInput;
-      const deckName = data.miauDeckInput;
-      // Get anki card's back
-      const response1 = await fetch("https://miau-miau-dict-backend.onrender.com/define/", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ word: selectedText, sentence: contextSentence, structure: structure}),
-      });
-      const data1 = await response1.json();
-      const clean_data = await data1[0] 
-      const back = clean_data.d // reverso de la carta
+        // retrieve local variables
+        const struct = data.miauAnkiInput;
+        const deckName = data.miauDeckInput;
+        const audioFilename = `${selectedText}_${deckName}_${codeLang}.mp3`.replace(/\s/g, "")
+        console.log(audioFilename)
+        // Get anki card's back
+        const back = await fetchCompletion(selectedText, contextSentence, struct)
 
-      // si no se ha generado el audio, se genera ahora,
-      if (!audio)
-      {
-        try {
-          const audioResponse = await fetch("https://miau-miau-dict-backend.onrender.com/tts/", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ text: selectedText, code:codeLang}),
-          });
-          console.log("fetch hecho")
-          const data = await audioResponse.json();
-          if (data.audio) {var audioFile = data.audio};
-        } catch (error) {
-          console.error("Error al obtener el audio:", error);
+        // si no se ha generado el audio, se genera ahora,
+        if (!audio) {
+          audioFile = await getAudio(selectedText, codeLang)
+          setAudio(audioFile)
+        }  else { // si ya hay audio, solo refedinir
+          var audioFile = audio
         }
-      }
-      // si ya hay audio, solo refedinir
-      else{
-        console.log("no new audio")
-        var audioFile = audio
-      }
-
-      // Guardar audio en la carpeta Anki
-      const audioFilename = `${selectedText}_test.mp3`
-      const storeMediaResponse = await fetch("http://localhost:8765", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          action: "storeMediaFile",
-          version: 6,
-          params: { filename: audioFilename, data: audioFile },
-        }),
-      });
-      const storeMediaResult = await storeMediaResponse.json();
-      // check for errors
-      if (!storeMediaResult.result) {
-        console.error("Error al guardar el audio en Anki");
-        return;
-      }
-      // generar new note
-      const addNoteResponse = await fetch("http://localhost:8765", { 
-        method: "POST", 
-        headers: { "Content-Type": "application/json" }, 
-        body: JSON.stringify({
-          "action": "addNote",
-          "version": 6,
-          "params": {
-            "note": {
-              "deckName": deckName,
-              "modelName": "Básico",
-              "fields": {
-                "Anverso": `[sound:${audioFilename}] ${selectedText}`,
-                "Reverso": back
-              },
-              "tags": ["miaumiau"],
-              "options": {
-                "allowDuplicate": false
-              }
-            }
-          }
-        }
-        )
-      });
-      const addNoteResult = await addNoteResponse.json();
-      console.log(addNoteResult); // todo correcto
-
+        storeMediaFiles(audioFilename, audioFile) // self-explanatory
+        // generar new note
+        noteID = await addNote(deckName, audioFilename, selectedText, back)
+        console.log(noteID)
     })} 
     // catch error
     catch (error) {
-      console.error("Error al agregar la flashcard:", error);
+      console.error("Error while adding card to anki: ", error);
     }
   }
   
@@ -226,13 +227,12 @@ function Popup() {
 
   return (
     <div
-      ref={popupRef}  // Asignamos el ref al contenedor del popup
-      className="popup-scroll"
+      ref={popupRef} 
+      className="popup-bubble"
       style={{
-        ...popupStyle,
         top: position.top,
         left: position.left,
-        transform: "translate(0, -100%)" // Mueve el popup hacia arriba su altura completa
+        transform: "translate(0, -100%)"
       }}
     >
       <div className="popup-header">
@@ -257,19 +257,5 @@ function Popup() {
   );
 }
 
-const popupStyle = {
-  position: "absolute",
-  backgroundColor: "#212121",
-  border: "3px solid #313131",
-  borderRadius: "21px 21px 21px 0px",
-  boxShadow: "0px 4px 10px rgba(0,0,0,0.2)",
-  zIndex: "1000",
-  fontSize: "12px",
-  maxWidth: "300px",
-  maxHeight: "140px",
-  overflowY: "auto",
-  paddingRight: "15px",
-  padding: "10px"
-};
 
 export default Popup;
